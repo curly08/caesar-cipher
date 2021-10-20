f2c183fcb3ac209a6d3034476f11bb14260d9198
curly08 <mattynoakes08@gmail.com>
2021-10-20 11:41:57 -0400

    Refactor caesar_cipher method

--- app.rb
+++ app.rb
@@ -1,24 +1,15 @@
 # frozen_string_literal: true
 
-def shift(arr, shift_factor)
-  arr.map do |value|
-    if value.between?(97, 122) && (value + shift_factor) > 122
-      (((value + shift_factor) - 122) + 96).chr
-    elsif value.between?(65, 90) && (value + shift_factor) > 90
-      (((value + shift_factor) - 122) + 96).chr
+def caesar_cipher(string, shift_factor)
+  unpacked_string = string.unpack('U*')
+  unpacked_string.map! do |value|
+    if value.between?(97, 122)
+      (value + shift_factor) > 122 ? (((value + shift_factor) - 122) + 96).chr : (value + shift_factor).chr
+    elsif value.between?(65, 90)
+      (value + shift_factor) > 90 ? (((value + shift_factor) - 90) + 64).chr : (value + shift_factor).chr
     else
-      (value + shift_factor).chr
+      value.chr
     end
   end
+  unpacked_string.join
 end
-
-def caesar_cipher(string, shift_factor)
-  result = shift(string.unpack('U*'), shift_factor).join
-  # result.capitalize if string == string.capitalize
-end
-
-# print "What message do you want to encode?\n"
-# string = gets.chomp
-# print "How many factors do you want to shift your message by?\n"
-# shift_factor = gets.chomp.to_i
-# caesar_cipher(string, shift_factor)
